51482b76ef7bd24581186dbad6af9b3f9901a455
Axel-Jacobsen <axelnj44@gmail.com>
2018-01-28 16:37:15 -0800

    weather fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,90 +1,137 @@
-// Used for compilation with Browserify. However, it is broken (for some reason, one of the packages are not being imported
-// properly). I manually fix this for now, but I would very much like to fix this with weather-js or browserify.
-// browserify scripts/weather.js -o bundle.js
-
-var weather = require('weather-js');
-
 function choosePic () {
-
-    var images = ['jvJVefx.jpg', 'A0aDza8.jpg', 'hLE28KQ.jpg', 'EeE9J.jpg', 'aZScqx6.jpg', 'PsDD5FK.jpg', 'AhgFudA.jpg', 'Y7tO223.jpg', 'GeljQRN.jpg', 'hZ52wIX.jpg', 'paiv3jU.jpg', 'd0k128jwxi101.jpg', 'BQ6KjPa.jpg', 'kWr2TCv.jpg', 'xtRaaY5.jpg', 'rEOPhmL.jpg', 'NO8ccpA.jpg', 'ra6M5zu.jpg', '6mnK4D0.jpg', '8Bq53xj.jpg', '1Qcwduq.jpg', 'f0ghN9G.jpg', 'Xik4G4c.jpg', 'Fq14Fsk.jpg', 'ti9jKcI.jpg', 'w7g4Tex.jpg', 'cgVCtwd.jpg', 'vnHhC5m.jpg', 'vBcVD4o.jpg', 'QqffsPx.jpg', 'q6Zfn7p.jpg', 'vwVqjMP.jpg', 'hbbpjec.jpg', 'AW16Y0i.jpg', 'YWMo1IJ.jpg',  'q5u0Ngd.jpg', 'L6w1YUN.jpg', 'UZH8X4x.jpg',  'afBGhhB.jpg', 'X0sd0Ho.jpg', '7SqL0BC.jpg',  '2dGXJp7.jpg', '3Dap2EL.jpg', 'sDsSu0m.jpg', 'hs6EUln.jpg', 'oSiYCN3.jpg', 'unbenannt3yywl.jpg', 'tqt2ewnd9m901.jpg', 'TNAG0Po.jpg', '2rQoBh8.jpg', 'GLi3zS6.jpg', 'SanFrancisco.jpg', 'VebP6Ol.jpg', '.Z7FLI6v.jpg.icloud', '5bKd1fd.jpg', 'uENexRC.jpg', 'eexbpm62e1601.jpg', 'QiUF7XB.jpg', 'GGYgJ0G.jpg', 'mQUd1tO.jpg', 'h8R5xMB.jpg', 'IW7I8dT.jpg', 'AdF1fVN.jpg', 'vQA6IsA.jpg', 'Spaceshuttle-Trough-Clouds.jpg']
+    var images = ['A0aDza8.jpg', 'hLE28KQ.jpg', 'EeE9J.jpg', 'aZScqx6.jpg', 'PsDD5FK.jpg', 'AhgFudA.jpg', 'Y7tO223.jpg', 'GeljQRN.jpg', 'hZ52wIX.jpg', 'paiv3jU.jpg', 'd0k128jwxi101.jpg', 'zlhxo4z.jpg', 'BQ6KjPa.jpg', 'hEQUZSu.jpg', 'kWr2TCv.jpg', 'xtRaaY5.jpg', 'rEOPhmL.jpg', 'NO8ccpA.jpg', 'ra6M5zu.jpg', '6mnK4D0.jpg', '8Bq53xj.jpg', '21nnfsH.jpg', '1Qcwduq.jpg', 'Xik4G4c.jpg', 'Fq14Fsk.jpg', 'ti9jKcI.jpg', 'w7g4Tex.jpg', 'cgVCtwd.jpg', 'vnHhC5m.jpg', 'vBcVD4o.jpg', 'QqffsPx.jpg', 'q6Zfn7p.jpg', 'vwVqjMP.jpg', 'hbbpjec.jpg', 'HjzhHvD.jpg', 'AW16Y0i.jpg', 'aleks-dahlberg-146811.jpg', 'YWMo1IJ.jpg', 'rDoBkEq.jpg', 'q5u0Ngd.jpg', 'L6w1YUN.jpg', 'UZH8X4x.jpg', '8a2XymE.jpg', 'afBGhhB.jpg', 'X0sd0Ho.jpg', 'fyFTaR2.jpg', '7SqL0BC.jpg', 'axytlMh.jpg', '2dGXJp7.jpg', '3Dap2EL.jpg', 'sDsSu0m.jpg', 'hs6EUln.jpg', 'oSiYCN3.jpg', 'unbenannt3yywl.jpg', 'tqt2ewnd9m901.jpg', 'HQLEbEg.jpg', 'vWHkb4I.jpg', 'TNAG0Po.jpg', '2rQoBh8.jpg', 'GLi3zS6.jpg', 'SanFrancisco.jpg', 'QC2RUkE.jpg', 'VebP6Ol.jpg', '5bKd1fd.jpg', 'uENexRC.jpg', 'eexbpm62e1601.jpg', 'QiUF7XB.jpg', 'GGYgJ0G.jpg', 'mQUd1tO.jpg', 'h8R5xMB.jpg', 'IW7I8dT.jpg', 'AdF1fVN.jpg', 'vQA6IsA.jpg', 'Spaceshuttle-Trough-Clouds.jpg']
 
     var randomNum = Math.floor(Math.random() * images.length);
     document.getElementById("centerImage").src = "WallPapers/" + images[randomNum];
 }
 
-function getWeather () {
-    weather.find({search: 'Vancouver, BC', degreeType: 'C'}, function(err, result) {
-        if(err) console.log(err);
+function metar () {
+    var k = "wx3f31992fe23995ee614ca37a59";
+    var n = "metar";
+    var a = "metar";
+    var tempGex = /(M?\d{2})\/(?:M?)\d{2}/;
+    var windGex = /(\d{3}(?:V\d{3})?)(\d+)(G\d+)?KT/;
+    var precip_forms = {
+        'DZ': 'wi-rain',
+        'SN': 'wi-snow',
+        'PL': 'wi-hail',
+        'GS': 'wi-hail',
+        'RA': 'wi-rain',
+        'SG': 'wi-snow',
+        'IC': 'wi-snow',
+        'SHRA': 'wi-showers',
+        'SHSN': 'wi-snow-wind',
+        'BLSN': 'wi-snow-wind',
+        'TS': 'wi-thunderstorm',
+        'RA': 'wi-rain',
+        '-RA': 'wi-sprinke',
+        '+RA': 'wi-rain-wind'
+    }
 
-        var skycode = result[0].current.skycode;
-        var skytext = result[0].current.skytext;
-        var temperature = result[0].current.temperature;
-        var wind = result[0].current.winddisplay;
+    var non_precip_forms = {
+        'CLR': 'wi-day-sunny',
+        'SKC': 'wi-day-sunny',
+        'FEW': 'wi-cloud',
+        'SCT': 'wi-cloud',
+        'BKN': 'wi-cloudy',
+        'OVC': 'wi-cloudy',
+        'FU': 'wi-smoke'
+    }
 
-        var iconTable = {
-            '31': 'wi-stars',
-            '32': 'wi-day-sunny',
-            '5': 'wi-rain-mix',
-            '6': 'wi-rain-mix',
-            '7': 'wi-sleet',
-            '8': 'wi-snowflake-cold',
-            '9': 'wi-showers',
-            '11': 'wi-rain',
-            '12': 'wi-rain',
-            '13': 'wi-snow',
-            '14': 'wi-snow',
-            '16': 'wi-snow',
-            '42': 'wi-snow',
-            '43': 'wi-snow',
-            '15': 'wi-snow-wind',
-            '18': 'wi-showers',
-            '40': 'wi-showers',
-            '19': 'wi-dust',
-            '20': 'wi-fog',
-            '21': 'wi-smog',
-            '22': 'wi-smoke',
-            '23': 'wi-strong-wind',
-            '24': 'wi-strong-wind',
-            '3': 'wi-thunderstorm',
-            '4': 'wi-thunderstorm',
-            '17': 'wi-thunderstorm',
-            '35': 'wi-thunderstorm',
-            '25': 'wi-snowflake-cold',
-            '26': 'wi-cloudy',
-            '27': 'wi-night-partly-cloudy',
-            '29': 'wi-night-partly-cloudy',
-            '33': 'wi-night-partly-cloudy',
-            '28': 'wi-day-cloudy',
-            '30': 'wi-day-cloudy',
-            '34': 'wi-day-cloudy',
-            '36': 'wi-hot',
-            '37': 'wi-thunderstorm',
-            '38': 'wi-thunderstorm',
-            '39': 'wi-showers',
-            '41': 'wi-snow-wind',
-            '45': 'wi-night-showers',
-            '46': 'wi-night-snow',
-            '47': 'wi-night-thunderstorm',
-        }
+    var non_precip_order = ['OVC', 'BKN', 'SCT', 'FEW', 'SKC', 'CLR'];
 
-        if (skycode in iconTable) {
-            var icon = iconTable[skycode];
-        } else {
-            var icon = "wi-train";
-        }
+    var clouds = {
+        'CI': 'Cirrus',
+        'CS': 'Cirrostratus',
+        'CC': 'Cirrocumulus',
+        'AS': 'Altostratus',
+        'AC': 'Altocumulus',
+        'ACC': 'Altocumulus castellanus',
+        'NS': 'Nimbostratus',
+        'ST': 'Stratus',
+        'SF': 'Stratus fractus',
+        'SC': 'Stratocumulus',
+        'CU': 'Cumulus',
+        'CF': 'Cumulus fractus',
+        'TCU': 'Heavy cumulus',
+        'CB': 'Cumulonimbus',
+        'SKC': 'Sky clear',
+        'CLR': 'Sky clear'
+    }
 
-        console.log(skycode); console.log(temperature); console.log(skytext); console.log(wind);
+    ! function(t, a, n) {
+        var d = new XMLHttpRequest;
+        d.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE && 200 === this.status) {
+                var t = JSON.parse(this.responseText);
+                metar = t['data'][0]['text'];
+                console.log(metar);
 
-        document.getElementById("weathericon").className = 'wi '+icon;
-        document.getElementById("temperature").innerHTML = temperature + " &deg;C";
-        document.getElementById("skytext").innerHTML     = skytext;
-        document.getElementById("wind").innerHTML        = wind;
-    });
+                var temp = metar.match(tempGex)[1].replace('0', '').replace('M', '-');
+                var wind = metar.match(windGex);
+                var windDir = wind[1].indexOf('V') < 0 ? wind[1] : wind[1].split('V')[0] + ' V ' + wind[1].split('V')[1];
+                var windSpeed = parseInt(wind[2] * 1.852).toString() + (wind[3] ? wind[3] : '');
+
+                var sky_cond;
+                
+                for (precip in precip_forms) {
+                    console.log(precip);
+                    if (metar.includes(precip)) {
+                    sky_cond = precip_forms[precip];
+                    break;
+                    }
+                };
+                if (!sky_cond){
+                    for (non_precip in non_precip_forms) {
+                    if (metar.includes(non_precip)) {
+                        sky_cond = non_precip_forms[non_precip];
+                        break;
+                    }
+                    }
+                }
+                if (!sky_cond) {
+                    sky_cond = 'wi-cloud-refresh';
+                }
+
+                var cloud_type;
+                for (cloud in clouds) {
+                    if (metar.includes(cloud)) {
+                    cloud_type = clouds[cloud];
+                    }
+                }
+                if (!cloud_type) {
+                    cloud_type = 'Horrible mystery clouds'
+                }
+
+                document.getElementById("weathericon").className = 'wi ' + sky_cond;
+                document.getElementById("temperature").innerHTML = temp + " &deg;C";
+                document.getElementById("wind").innerHTML        = windSpeed + ' km/h ' + windDir + '&deg;';
+                document.getElementById("clouds").innerHTML      = cloud_type;
+
+                }
+        }, d.open("GET", "https://api.checkwx.com/" + a.toLowerCase() + "/" + t, !0), d.setRequestHeader("X-API-KEY", k), d.send()
+    }('CYVR' || "", n, t[a])
+}
+
+function checkTime(i) {
+    return (i < 10) ? "0" + i : i;
+}
+
+function startTime() {
+    var today = new Date(),
+        h = checkTime(today.getHours()),
+        m = checkTime(today.getMinutes()),
+        s = checkTime(today.getSeconds());
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
 }
 
 function mainFunc () {
-    getWeather();
+    startTime();
     choosePic();
+    metar();
 }
 
 window.onload = mainFunc;
