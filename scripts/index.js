02222db164f73c9b6746386207347fb9ae16ce7d
Axel-Jacobsen <axelnj44@gmail.com>
2018-02-15 20:11:52 -0800

    FIx ahhhhghghghg

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,28 @@
 function choosePic () {
 	
-    var images = ['xenqt1xpjbuz.jpg', 'hLE28KQ.jpg', 'EeE9J.jpg', 'f4uy8su3kp201.jpg', 'aZScqx6.jpg', 'PsDD5FK.jpg', 'AhgFudA.jpg', 'Y7tO223.jpg', 'GeljQRN.jpg', 'oiZfq.jpg', 'LVaoA.jpg', 'hZ52wIX.jpg', 'paiv3jU.jpg', 'd0k128jwxi101.jpg', 'zlhxo4z.jpg', 'BQ6KjPa.jpg', 'hEQUZSu.jpg', 'kWr2TCv.jpg', 'rZhTV.jpg', 'xtRaaY5.jpg', 'rEOPhmL.jpg', 'NO8ccpA.jpg', 'ra6M5zu.jpg', '6mnK4D0.jpg', '8Bq53xj.jpg', '21nnfsH.jpg', '1Qcwduq.jpg', '6zowermr3uc01.jpg', 'Xik4G4c.jpg', 'Fq14Fsk.jpg', 'ti9jKcI.jpg', 'w7g4Tex.jpg', 'cgVCtwd.jpg', 'vnHhC5m.jpg', 'vBcVD4o.jpg', 'QqffsPx.jpg', 'q6Zfn7p.jpg', 'vwVqjMP.jpg', 'hbbpjec.jpg', 'AW16Y0i.jpg', 'aleks-dahlberg-146811.jpg', 'YWMo1IJ.jpg', 'q5u0Ngd.jpg', 'L6w1YUN.jpg', 'UZH8X4x.jpg', 'afBGhhB.jpg', 'X0sd0Ho.jpg', 'fyFTaR2.jpg', '7SqL0BC.jpg', 'axytlMh.jpg', '2dGXJp7.jpg', '3Dap2EL.jpg', 'sDsSu0m.jpg', 'hs6EUln.jpg', 'oSiYCN3.jpg', 'unbenannt3yywl.jpg', 'tqt2ewnd9m901.jpg', 'HQLEbEg.jpg', 'vWHkb4I.jpg', 'TNAG0Po.jpg', '2rQoBh8.jpg', 'GLi3zS6.jpg', 'bqrrjo2v1uc01.jpg', 'SanFrancisco.jpg', 'QC2RUkE.jpg', 'VebP6Ol.jpg', 'uENexRC.jpg', 'GGYgJ0G.jpg', 'mQUd1tO.jpg', 'h8R5xMB.jpg', 'IW7I8dT.jpg', 'AdF1fVN.jpg', 'vQA6IsA.jpg', 'Spaceshuttle-Trough-Clouds.jpg'];
+    let images = ['xenqt1xpjbuz.jpg', 'streamline.jpeg', 'hLE28KQ.jpg', 'EeE9J.jpg', 'f4uy8su3kp201.jpg', 'aZScqx6.jpg', 'PsDD5FK.jpg', 'AhgFudA.jpg', 'Y7tO223.jpg', 'GeljQRN.jpg', 'oiZfq.jpg', 'LVaoA.jpg', 'hZ52wIX.jpg', 'paiv3jU.jpg', 'd0k128jwxi101.jpg', 'zlhxo4z.jpg', '.DS_Store', 'download.jpg', 'BQ6KjPa.jpg', 'hEQUZSu.jpg', 'kWr2TCv.jpg', 'rZhTV.jpg', 'xtRaaY5.jpg', '.j5qGtoB.jpg.icloud', '4fdxgR2.jpg', 'rEOPhmL.jpg', 'NO8ccpA.jpg', 'ra6M5zu.jpg', '6mnK4D0.jpg', '8Bq53xj.jpg', 'pQS06lt.jpg', '21nnfsH.jpg', '1Qcwduq.jpg', '6zowermr3uc01.jpg', 'Xik4G4c.jpg', '.fvMx4o6.jpg.icloud', 'Fq14Fsk.jpg', 'ti9jKcI.jpg', 'w7g4Tex.jpg', 'cgVCtwd.jpg', 'vnHhC5m.jpg', 'vBcVD4o.jpg', 'QqffsPx.jpg','q6Zfn7p.jpg', 'vwVqjMP.jpg', 'hbbpjec.jpg', 'AW16Y0i.jpg', 'aleks-dahlberg-146811.jpg', 'YWMo1IJ.jpg', 'q5u0Ngd.jpg', 'nyxv1wc26uf01.jpg', 'L6w1YUN.jpg', 'UZH8X4x.jpg', 'afBGhhB.jpg', 'X0sd0Ho.jpg', 'fyFTaR2.jpg', '7SqL0BC.jpg', 'axytlMh.jpg', '2dGXJp7.jpg', '3Dap2EL.jpg', 'sDsSu0m.jpg', 'hs6EUln.jpg', 'oSiYCN3.jpg', 'unbenannt3yywl.jpg', 'tqt2ewnd9m901.jpg', 'HQLEbEg.jpg', 'vWHkb4I.jpg', 'TNAG0Po.jpg','zk4vozzbq3e01.jpg', '2rQoBh8.jpg', 'GLi3zS6.jpg', 'bqrrjo2v1uc01.jpg', 'SanFrancisco.jpg', 'QC2RUkE.jpg', 'VebP6Ol.jpg', 'SpaceEx.png.jpg', 'uENexRC.jpg', 'kLES8HF.jpg', 'GGYgJ0G.jpg', 'mQUd1tO.jpg', 'h8R5xMB.jpg', '5gah229mbqd01.jpg', '9ui2qmc9fqd01.jpg', '38387192785_da1893f6d1_o.jpg', 'IW7I8dT.jpg', 'g4eou5yk0sf01.jpg', 'AdF1fVN.jpg', 'vQA6IsA.jpg', 'Spaceshuttle-Trough-Clouds.jpg', 'Attachment-1.jpeg'];
 
-    var randomNum = Math.floor(Math.random() * images.length);
-    document.getElementById("centerImage").src = "WallPapers/" + images[randomNum];
+    let tries = 0;
+    let complete = false;
+    let randomNum = () => Math.floor(Math.random() * images.length);
+    do {
+        try {
+            document.getElementById("centerImage").src = "WallPapers/" + images[randomNum()];
+            complete = true;
+        } catch (err) {
+            ++tries;
+        }
+    } while (!complete && tries < 3)
     // document.getElementById("back").style.backgroundImage = "url(" + "WallPapers/" + images[randomNum] + ")";
 }
 
 function metar () {
-    var k = "wx3f31992fe23995ee614ca37a59";
-    var n = "metar";
-    var a = "metar";
-    var tempGex = /(M?\d{2})\/(?:M?)\d{2}/;
-    var windGex = /(\d{3}(?:V\d{3})?)|(?:VRB)(\d+)(G\d+)?KT/;
-    var precip_forms = {
+    const k = "wx3f31992fe23995ee614ca37a59";
+    const n = "metar";
+    const a = "metar";
+    const tempGex = /(M?\d{2})\/(?:M?)\d{2}/;
+    const windGex = /(\d{3}(?:V\d{3})?)(\d+)(G\d+)?KT/;;
+    const precip_forms = {
         'DZ': 'wi-rain',
         'SN': 'wi-snow',
         'PL': 'wi-hail',
@@ -30,7 +39,7 @@ function metar () {
         '+RA': 'wi-rain-wind'
     }
 
-    var non_precip_forms = {
+    const non_precip_forms = {
         'CLR': 'wi-day-sunny',
         'SKC': 'wi-day-sunny',
         'FEW': 'wi-cloud',
@@ -40,9 +49,9 @@ function metar () {
         'FU': 'wi-smoke'
     }
 
-    var non_precip_order = ['OVC', 'BKN', 'SCT', 'FEW', 'SKC', 'CLR'];
+    const non_precip_order = ['OVC', 'BKN', 'SCT', 'FEW', 'SKC', 'CLR'];
 
-    var clouds = {
+    const clouds = {
         'CI': 'Cirrus',
         'CS': 'Cirrostratus',
         'CC': 'Cirrocumulus',
@@ -62,24 +71,19 @@ function metar () {
     }
 
     ! function(t, a, n) {
-        var d = new XMLHttpRequest;
+        let d = new XMLHttpRequest;
         d.onreadystatechange = function() {
             if (this.readyState === XMLHttpRequest.DONE && 200 === this.status) {
-                var t = JSON.parse(this.responseText);
+                let t = JSON.parse(this.responseText);
                 metar = t['data'][0]['text'];
 		console.log(metar);
-                var temp = metar.match(tempGex)[1].replace('M', '-');
-                var wind = metar.match(windGex);
-                var windSpeed = parseInt(wind[2] * 1.852).toString() + (wind[3] ? wind[3] : '');
+                let temp = metar.match(tempGex)[1].replace('M', '-');
+                let wind = metar.match(windGex);
+                let windSpeed = parseInt(wind[2] * 1.852).toString() + (wind[3] ? wind[3] : '');
+                console.log(wind);
+                let windDir = wind[1].indexOf('V') < 0 ? wind[1] : wind[1].split('V')[0] + ' V ' + wind[1]
 
-                var windDir;
-                if (wind[1].includes('VRB')) {
-                    windDir = 'Variable'
-                } else {
-                    windDir = wind[1].indexOf('V') < 0 ? wind[1] : wind[1].split('V')[0] + ' V ' + wind[1].split('V')[1];
-                }
-
-                var sky_cond;
+                let sky_cond;
                 for (precip in precip_forms) {
                     if (metar.includes(precip)) {
                     sky_cond = precip_forms[precip];
@@ -98,7 +102,7 @@ function metar () {
                     sky_cond = 'wi-cloud-refresh';
                 }
 
-                var cloud_type;
+                let cloud_type;
                 for (cloud in clouds) {
                     if (metar.includes(cloud)) {
                     cloud_type = clouds[cloud];
@@ -123,7 +127,7 @@ function checkTime(i) {
 }
 
 function startTime() {
-    var today = new Date(),
+    let today = new Date(),
         h = checkTime(today.getHours()),
         m = checkTime(today.getMinutes()),
         s = checkTime(today.getSeconds());
@@ -138,5 +142,5 @@ function mainFunc () {
     choosePic();
     metar();
 }
-
+// Colours: RGB = (x, (1.618) * x, (1.618^2) * x), 0 < 1.618^2 * x < 256
 window.onload = mainFunc;
